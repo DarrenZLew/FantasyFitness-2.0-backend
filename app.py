671aa3c322fcd1b643fd4de9893fc748d4fbd3b1
Darren Lew <darrenzlew@gmail.com>
2019-09-15 12:56:41 -0500

    Add more db stuff

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ POSTGRES = {
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pass123@localhost:5433/fantasy-fitness'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Init db
@@ -28,10 +27,13 @@ ma = Marshmallow(app)
 
 # Member Class/Model
 class Member(db.Model):
+    __tablename__ = 'members'
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), unique=True)
     last_name = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(80), unique=True)
+    leagues = db.relationship('Member_league', back_populates='member')
 
     def __init__(self, first_name, last_name, email):
         self.first_name = first_name
@@ -78,9 +80,12 @@ def get_member(id):
 
 # League Class/Model
 class League(db.Model):
+    __tablename__ = 'leagues'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     type = db.Column(db.String(80))
+    members = db.relationship('Member_league', back_populates='league')
 
     def __init__(self, name, type):
         self.name = name
@@ -121,10 +126,27 @@ def get_league(id):
   league = League.query.get(id)
   return league_schema.jsonify(league)
 
+#################################################s################
+
+# Member League Class/Model
+class Member_league(db.Model):
+    __tablename__ = 'member_league'
+
+    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'), primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('members.id'), primary_key=True)
+    privilege = db.Column(db.String(80))
+
+    def __init__(self, name, type):
+        self.league_id = league_id
+        self.member_id = member_id
+        self.privilege = privilege
+
 #################################################################
 
 # Activity Class/Model
 class Activity(db.Model):
+    __tablename__ = 'activities'
+
     id = db.Column(db.Integer, primary_key=True)
     league_id = db.Column(db.Integer)
     points = db.Column(db.Integer)
@@ -181,22 +203,24 @@ def get_activity(id):
 
 # Season Class/Model
 class Season(db.Model):
+    __tablename__ = 'seasons'
+
     id = db.Column(db.Integer, primary_key=True)
     league_id = db.Column(db.Integer)
     weeks = db.Column(db.Integer)
     disabled = db.Column(db.Boolean)
-    # start_date = db.Column(db.Date)
+    start_date = db.Column(db.Date)
 
-    def __init__(self, league_id, weeks, disabled):
+    def __init__(self, league_id, weeks, disabled, start_date):
         self.league_id = league_id
         self.weeks = weeks
         self.disabled = disabled
-        # self.start_date = start_date
+        self.start_date = start_date
 
 # Season Schema
 class SeasonSchema(ma.Schema):
   class Meta:
-    fields = ('id', 'league_id', 'weeks', 'disabled')
+    fields = ('id', 'league_id', 'weeks', 'disabled', 'start_date')
 
 # Init schema
 season_schema = SeasonSchema()
@@ -208,9 +232,9 @@ def add_season():
   league_id = request.json['league_id']
   weeks = request.json['weeks']
   disabled = False
-#   start_date = request.json['start_date']
+  start_date = request.json['start_date']
 
-  new_season = Season(league_id, weeks, disabled)
+  new_season = Season(league_id, weeks, disabled, start_date)
 
   db.session.add(new_season)
   db.session.commit()
